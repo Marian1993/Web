8f954960e77191a8d42bd58b3bce12a55c4c10ef
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-10-28 19:30:50 +0200

    home

--- carreto.php
+++ carreto.php
@@ -11,34 +11,61 @@ include_once"sessio.php";
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<table class="table table-hover">
-  <thead>
-    <tr>
-      <th scope="col">#</th>
-      <th scope="col">First</th>
-      <th scope="col">Last</th>
-      <th scope="col">Handle</th>
-    </tr>
-  </thead>
-  <tbody>
-    <tr>
-      <th scope="row">1</th>
-      <td>Mark</td>
-      <td>Otto</td>
-      <td>@mdo</td>
-    </tr>
-    <tr>
-      <th scope="row">2</th>
-      <td>Jacob</td>
-      <td>Thornton</td>
-      <td>@fat</td>
-    </tr>
-    <tr>
-      <th scope="row">3</th>
-      <td colspan="2">Larry the Bird</td>
-      <td>@twitter</td>
-    </tr>
-  </tbody>
-</table>
+<div class="contaniner">
+  <div class="row">
+    <div class="col-1 text-center">
+      <p><a href="llista.php"><br>Tornar<br><br><br></a></p>
+    </div>
+  </div>
+  <div class="row">
+    <div class="col-12 text-center">
+      <table class="table">
+        <thead>
+          <tr>
+            <th scope="col">N Producto</th>
+            <th scope="col">Imagen</th>
+            <th scope="col">Nombre</th>
+            <th scope="col">Precio</th>
+          </tr>
+        </thead>
+        <tbody>
+        <?php
+          $sql = "SELECT id, nom FROM Hamburguesa;";
+          $result = $conn->query($sql);
+          $total = 0;
+
+          if ($result->num_rows < 3) {
+              // output data of each row
+              while($row = $result->fetch_assoc()) {
+                $total += $row["preu"];
+          ?>
+          <tr>
+            <th scope="row">1</th>
+            <td>
+              <a href="http://192.168.0.111/Web/producte.php?id=<?php echo $row["id"];?>">
+                <img src="./Img/<?php echo $row["id"];?>.jpg" class="rounded" height="150px" width="200px">
+              </a>
+            </td>
+            <td><?php echo $row["nom"];?></td>
+            <td><?php echo $row["preu"];?>€</td>
+          </tr>
+          <?php
+          }
+              } else {
+              echo "0 results";
+          } 
+          mysqli_close($conn);
+          ?>
+        </tbody>
+      </table>  
+    </div>
+  </div>
+  <div class="row">
+    <div class="col align-self-end">
+    <p> Total: <?php echo $total;?>€<br><br></p>
+    </div>
+  </div>
+  </div>
+</div>
 </body>
 </html> 
