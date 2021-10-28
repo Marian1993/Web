d1302d66934bc2c3e5b2a7e7e34308c724bdd969
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-10-28 12:42:37 +0200

    class

--- producte2.php
+++ producte2.php
@@ -21,27 +21,28 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Llista</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
     <div class="contaniner">
         <div class="row">
+            <div class="col-9">
             <p><a href="llista.php">Tornar</a></p>
+            </div>
         </div>
-    </div>
     <?php
     $sql = "SELECT id, nom, tipusCarn, preu FROM Hamburguesa where id =" . $_GET["id"] . ";";
     $result = $conn->query($sql);
     if ($result->num_rows > 0) {
         // output data of each row
         $row = $result->fetch_assoc();
-        ?>
-    <div class="container">
-        <div class="row"><img src="./Img/<?php echo $row["id"];?>.jpg" height="150px" width="200px"></div>
-        <div class="row"><?php echo $row["nom"];?></div>
-        <div class="row"><?php echo $row["preu"];?></div>
-        <div class="row"><?php echo $row["tipusCarn"];?></div>
-        <div class="row">
-            <button>Afegir al carreto</button>
+    ?>
+        <div class="col-6 text-center"><img src="./Img/<?php echo $row["id"];?>.jpg" class="rounded" height="150px" width="200px"></div>
+        <div class="col-4">
+            <h1><?php echo $row["nom"];?></h1>
+            <h3><?php echo $row["tipusCarn"];?></h3>
+            <p><?php echo $row["preu"];?>€</p>
+            <button>Afegir al carreto</button>        
         </div>
     </div>
     <?php
