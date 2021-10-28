665bae4e950132ababd906a5450d506ba5a103c2
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-10-28 20:27:23 +0200

    home

--- carreto.php
+++ carreto.php
@@ -34,7 +34,7 @@ include_once"sessio.php";
         $result = $conn->query($sql);
         $total = 0;
 
-        if ($result->num_rows > 3) {
+        if ($result->num_rows < 2) {
             // output data of each row
             while($row = $result->fetch_assoc()) {
               $total += $row["preu"];
