fd6cd668fe50206135d8a6349633aa8eb72bb31b
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-11-02 18:35:14 +0100

    home

--- llista.php
+++ llista.php
@@ -13,7 +13,7 @@
       $lang = $_COOKIE['idioma']; 
     } 
   } else{
-    $_COOKIE = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
+    $_COOKIE['idioma'] = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
   }
   include("lang/".$lang.".php");
 ?>
@@ -27,19 +27,17 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-<header>
-    <nav class="navbar navbar-light bg-light justify-content-between">
-    <form class="form-inline" method="GET">
-        <label class="mr-sm-2" for="inlineFormCustomSelectPref"><?php echo $lang["cambiar_idioma"]; ?></label>
-        <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="idioma" >
-            <option ></option>
-            <option value="es"><?php echo $lang["opcion_2"];?></option>
-            <option value="ca"><?php echo $lang["opcion_3"];?></option>
-        </select>
-        <button type="submit" class="btn btn-primary"><?php echo $lang["cambiar"]; ?></button>
-</form>
-</nav>
-  </header>
+    <header>
+        <nav class="navbar navbar-light bg-light justify-content-between">
+            <label class="mr-sm-2" for="inlineFormCustomSelectPref"><?php echo $lang["cambiar_idioma"]; ?></label>
+            <select class="custom-select mb-2 mr-sm-2 mb-sm-0" name="idioma" >
+                <option ></option>
+                <option value="es"><?php echo $lang["opcion_2"];?></option>
+                <option value="ca"><?php echo $lang["opcion_3"];?></option>
+            </select>
+            <button type="submit" class="btn btn-primary"><?php echo $lang["cambiar"]; ?></button>
+        </nav>
+    </header>
 <div class="container">
     <div class="row">
         <div class="col-12">
