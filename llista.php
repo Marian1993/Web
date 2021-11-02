b3cd791ec287289b3aef5ea53cea150e8dfae44d
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-11-02 19:25:07 +0100

    home

--- llista.php
+++ llista.php
@@ -4,18 +4,15 @@
   // una cookie con el idioma 
   if(isset($_GET['idioma'])){ 
     $lang = $_GET['idioma']; 
-    echo "1111111";
   } 
   elseif(isset($_COOKIE['idioma'])){ 
   // Miri que exista el archivo del idioma 
     if(file_exists("lang/".$_COOKIE['idioma'].".php")){ 
       $lang = $_COOKIE['idioma']; 
     } 
-    echo "22222222";
   } else{
     setcookie ("idioma", substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2), time () + 3600*24);
     $lang = substr($_SERVER['HTTP_ACCEPT_LANGUAGE'], 0, 2);
-    echo "ho detecta";
   }
   include("lang/".$lang.".php");
 ?>
