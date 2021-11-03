0f623b20d7ad1ec8e311d88154fc61ca1f49206c
DESKTOP-IG4JCL5\maria <marianmartorell.93@gmail.com>
2021-11-03 21:25:12 +0100

    home

--- carreto2.php
+++ carreto2.php
@@ -35,8 +35,7 @@ session_start();
         $sql = "SELECT id, nom, preu FROM Hamburguesa limit 2;";
         $result = $conn->query($sql);
         $total = 0;
-        echo $_SESSION["carrito"] = $_GET['id'];
-
+        echo $_SESSION["carrito"];
 
         if ($result->num_rows > 0) {
 
